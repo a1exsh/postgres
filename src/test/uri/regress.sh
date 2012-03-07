943d4d49655d06eb0066700efb07d43f7007e7e5
Alex Shulgin <ash@commandprompt.com>
2012-03-07 12:04:38 -0800

    Fix URI regress test: don't subst PGPORT/USER to sql.

--- src/test/uri/regress.sh
+++ src/test/uri/regress.sh
@@ -10,37 +10,15 @@ fi
 
 echo "Running libpq URI support test..."
 
-while read uri
+while read line
 do
-    "${BINDIR}/psql" -d "$uri" -At -c "SELECT '$uri'"
-done >regress.out <<EOF
-postgresql://${PGUSER}@localhost:${PGPORT}/${PGUSER}
-postgresql://${PGUSER}@localhost/${PGUSER}
-postgresql://localhost:${PGPORT}/${PGUSER}
-postgresql://localhost/${PGUSER}
-postgresql://${PGUSER}@localhost:${PGPORT}/
-postgresql://${PGUSER}@localhost/
-postgresql://localhost:${PGPORT}/
-postgresql://localhost:${PGPORT}
-postgresql://localhost/${PGUSER}
-postgresql://localhost/
-postgresql://localhost
-postgresql://
-postgresql://%6Cocalhost/
-postgresql://localhost/${PGUSER}?user=${PGUSER}
-postgresql://localhost/${PGUSER}?user=${PGUSER}&port=${PGPORT}
-postgresql://localhost/${PGUSER}?user=${PGUSER}&port=${PGPORT}
-postgresql://localhost:${PGPORT}?user=${PGUSER}
-postgresql://localhost?user=${PGUSER}
-postgresql://localhost?uzer=
-postgresql://localhost?
-postgresql://[::1]:${PGPORT}/${PGUSER}
-postgresql://[::1]/${PGUSER}
-postgresql://[::1]/
-postgresql://[::1]
-postgres://
-postgres:///tmp
-EOF
+    # Frist, expand variables in the test URI line.
+    uri=$(eval echo "$line")
+
+    # But SELECT the original line, so test result doesn't depend on
+    # the substituted values.
+    "${BINDIR}/psql" -d "$uri" -At -c "SELECT '$line'"
+done <regress.in >regress.out
 
 if diff -c expected.out regress.out >regress.diff; then
     echo "========================================"
